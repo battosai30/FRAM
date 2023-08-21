35ab781fe7298d0b7f3d454b62ceb1cc70f6dc12
battosai30 <batto@hotmail.fr>
2023-08-21 11:50:55 +0200

    Update FRAM.cpp

--- FRAM.cpp
+++ FRAM.cpp
@@ -1,6 +1,6 @@
 #include "FRAM.h"
 
-FRAM::FRAM(byte Address) {
+FRAM::FRAM(uint8_t Address) {
 
 Wire.begin();
 _i2cAddress= (0b101000 | Address)<<1;
@@ -11,7 +11,7 @@ FRAM::~FRAM() {
 
 }
 
-void FRAM::WriteByte(byte Page,byte Address,byte Data) {
+void FRAM::WriteByte(uint8_t Page, uint8_t Address, uint8_t Data) {
  
   Wire.beginTransmission(_i2cAddress | (Page&1));
   Wire.write(Address);
@@ -21,7 +21,7 @@ void FRAM::WriteByte(byte Page,byte Address,byte Data) {
 }
 
 
-byte FRAM::ReadByte(byte Page,byte Address) {
+uint8_t FRAM::ReadByte(uint8_t Page,uint8_t Address) {
  
   ReadBurst(Page,Address, 1);
      
@@ -29,7 +29,7 @@ byte FRAM::ReadByte(byte Page,byte Address) {
   
 }
 
-int FRAM::ReadInt(byte Page,byte Address) {
+int16_t FRAM::ReadInt(uint8_t Page, uint8_t Address) {
 
   ReadBurst(Page,Address, 2);
      
@@ -37,48 +37,48 @@ int FRAM::ReadInt(byte Page,byte Address) {
 
 }
 
-void FRAM::WriteInt(byte Page, byte Address, int Data) {
+void FRAM::WriteInt(uint8_t Page, uint8_t Address, int16_t Data) {
 
-byte Data_Array[2]={Data & 0xFF, (Data>>8) & 0xFF};
+  uint8_t Data_Array[2]={Data & 0xFF, (Data>>8) & 0xFF};
 
-WriteBurst(Page, Address, Data_Array, 2);
+  WriteBurst(Page, Address, Data_Array, 2);
 
 }
 
-long FRAM::ReadLong(byte Page,byte Address) {
+int32_t FRAM::ReadLong(uint8_t Page, uint8_t Address) {
 
   ReadBurst(Page,Address, 4);
      
-  return long(Wire.read()) | long(Wire.read())<<8 | long(Wire.read())<<16 | long(Wire.read())<<24 ;
+  return int32_t(Wire.read()) | int32_t(Wire.read())<<8 | int32_t(Wire.read())<<16 | int32_t(Wire.read())<<24 ;
 
 }
 
-void FRAM::WriteLong(byte Page, byte Address, long Data) {
+void FRAM::WriteLong(uint8_t Page, uint8_t Address, int32_t Data) {
 
-byte Data_Array[4]={Data & 0xFF, (Data>>8) & 0xFF, (Data>>16) & 0xFF, (Data>>24) & 0xFF};
+ uint8_t Data_Array[4]={Data & 0xFF, (Data>>8) & 0xFF, (Data>>16) & 0xFF, (Data>>24) & 0xFF};
 
-WriteBurst(Page, Address, Data_Array, 4);
+ WriteBurst(Page, Address, Data_Array, 4);
 
 }
 
-void FRAM::ReadBurst(byte Page, byte Address, int NumberOfByte) {
+void FRAM::ReadBurst(uint8_t Page, uint8_t Address, uint16_t NumberOfByte) {
 
   Wire.beginTransmission(_i2cAddress | (Page&1));
   Wire.write(Address);
   Wire.endTransmission();  
   Wire.requestFrom(_i2cAddress | (Page&1), NumberOfByte);
   
- }
+}
  
- void FRAM::WriteBurst(byte Page, byte Address, byte* Data, int NumberOfByte) {
+ void FRAM::WriteBurst(uint8_t Page, uint8_t Address, uint8_t* Data, uint16_t NumberOfByte) {
 
   Wire.beginTransmission(_i2cAddress | (Page&1));
   Wire.write(Address);
   
   for(int i=0;i<NumberOfByte;i++) {
-  Wire.write(Data[i]);
+   Wire.write(Data[i]);
   }
   
   Wire.endTransmission(); 
   
- }
+}
